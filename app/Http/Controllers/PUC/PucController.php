e4309eaae0b686c19e89a9063276950930644e7f
luis930114 <luis930114@gmail.com>
2017-02-10 12:09:11 -0500

    controladores
    Se creo los controladores para la parte de talleres , PUC y materia el
    cual estan contenidas en diferentes carpetas que contiene los
    controladores que haran parte de las relaciones  entre otras
    funcionalidades

---
+++ app/Http/Controllers/PUC/PucController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\PUC;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PucController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Taller/PreguntaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Taller;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PreguntaController extends Controller
+{
+    //
+}
