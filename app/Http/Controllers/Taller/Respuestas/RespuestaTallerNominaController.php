901df9c21672a3875fdb8b6b08d75abb31e34942
luis930114 <luis930114@gmail.com>
2017-02-10 20:04:17 -0500

    Nuevos Controladores
    * Se adicionaron nuevos controladores para la aplicación

---
+++ app/Http/Controllers/Taller/Respuestas/RespuestaTallerNominaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Taller\Respuestas;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RespuestaTallerNominaController extends Controller
+{
+    //
+}
