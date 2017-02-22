6fc50d3ca9431f0c291a1d9ab96eac9b56e2c01c
luis930114 <luis930114@gmail.com>
2017-02-21 23:12:02 -0500

    Avances y creación de la vista del profesor
    *Se creo una nueva vista llamada nav_admin
    
    * Se crearon nuevas llaves de traducción para la anterior vista
    
    * Se arreglan varias etiquetas de traducción para restablecer contraseña

--- resources/views/admin/template/partials/footer.blade.php
+++ resources/views/admin/template/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
 	<div class="container">
-    	<p class="text-muted credit">{{ trans('messages.footer') }} <a target="_blank" href="https://github.com/admont28">Andrés David Montoya Aguirre</a> {{ trans('messages.and') }}<a target="_blank" href="https://github.com/luis930114">Luis Felipe Hernández Gómez</a>.
+    	<p class="text-muted credit">{{ trans('messages.footer') }} <a target="_blank" href="https://github.com/admont28">Andrés David Montoya Aguirre</a> {{ trans('messages.and') }} <a target="_blank" href="https://github.com/luis930114">Luis Felipe Hernández Gómez</a>.
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				 <ul class="nav navbar-nav navbar-right">
 					 <li><a href="{{ url('lang', ['en']) }}">En</a></li>
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Restablecer la contraseña</div>
+                <div class="panel-heading">{{ trans('messages.restablecer_contrasena') }}</div>
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
@@ -20,7 +20,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label" >Correo Electronico</label>
+                            <label for="email" class="col-md-4 control-label" >{{ trans('messages.correo') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
@@ -36,7 +36,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                  Enviar contraseña 
+                                  {{ trans('messages.enviar_contrasena') }}
                                 </button>
                             </div>
                         </div>
