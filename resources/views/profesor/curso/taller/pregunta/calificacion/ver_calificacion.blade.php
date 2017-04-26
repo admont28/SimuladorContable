532a919ad903eeab30074221b39dc2b1245ec44b
luis930114 <luis930114@gmail.com>
2017-04-25 23:56:55 -0500

    Vista de calificación para los usuarios que han respondido el taller.
    *Se muestra el datatable con los usuarios que han respondido un taller.

--- resources/views/profesor/curso/taller/pregunta/calificacion/ver_calificacion.blade.php
+++ resources/views/profesor/curso/taller/pregunta/calificacion/ver_calificacion.blade.php
@@ -1,17 +1,35 @@
-@extends('profesor.template.main')
-
-@section('title-head', 'Ver calificacion')
-
-@section('title', 'taller <strong>'.$taller->tall_id.'...</strong>')
-
-@section('active','#profesor-curso')
-
-@section('content')
-
-    <div class="row">
-        <div class="table-responsive">
-            <p>{{$pregunta->preg_texto}}</p>
-        </div>
+<br>
+<div class="row">
+    <div  class="table-responsive" >
+        <table class="table" id="usuario-table" >
+            <thead>
+                <tr>
+                    <td><strong>ID</strong></td>
+                    <td><strong>Nombre</strong></td>
+                    <td><strong>Opciones</strong></td>
+                </tr>
+            </thead>
+        </table>
     </div>
+</div>
+<br>
 
-@endsection
+@push('scripts')
+    <script type="text/javascript">
+        $(function() {
+            $('#usuario-table').DataTable({
+                "processing": true,
+                "serverSide": true,
+                "ajax": "{{ route('profesor.curso.taller.pregunta.respuesta.calificacion',['tall_id' => $taller->tall_id,'curs_id'=>$taller->curs_id]) }}",
+                "columns" : [
+                    {data: 'usua_id', name: 'usua_id', width: '5%'},
+                    {data: 'usua_nombre', name: 'usua_nombre', width: '50%'},
+                    {data: 'opciones', name: 'action', orderable: false, searchable: false, width: '30%'}
+                ],
+                "language" : {
+                    "url" : "//cdn.datatables.net/plug-ins/1.10.12/i18n/Spanish.json"
+                }
+            });
+        });
+    </script>
+@endpush
