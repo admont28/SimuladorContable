bc5546379e020cf6ad1ebdb006955c2a2d447b5e
Andrés David Montoya Aguirre <admont28@gmail.com>
2017-05-11 16:28:16 -0500

    [Incompleta] Funcionalidad de solucionar un taller kardex.
    * Se implementa el javascript que manejará la tabla de kardex con las reglas de negocio dadas en la plantilla de excel.
    * Se implementa el envío de la información para guardar un taller kardex.

--- resources/views/estudiante/curso/taller/ver_tallerpractico.blade.php
+++ resources/views/estudiante/curso/taller/ver_tallerpractico.blade.php
@@ -434,6 +434,29 @@
                             </div>
                         @elseif (isset($tallerPractico->tallerKardex))
                             <div class="row">
+                                <div class="form-horizontal">
+                                    <div class="form-group">
+                                        <label for="articulo_taller_kardex" class="col-lg-2 control-label">Artículo:</label>
+                                        <div class="col-lg-10">
+                                            <input type="text" class="form-control" placeholder="Ingrese el artículo" name="articulo_taller_kardex"/>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="direccion_taller_kardex" class="col-lg-2 control-label">Dirección:</label>
+                                        <div class="col-lg-10">
+                                            <input type="text" class="form-control" placeholder="Ingrese la dirección" name="direccion_taller_kardex"/>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="proveedores_taller_kardex" class="col-lg-2 control-label">Proveedores:</label>
+                                        <div class="col-lg-10">
+                                            <input type="text" class="form-control" placeholder="Ingrese los proveedores" name="proveedores_taller_kardex"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
                                 <div class="table-responsive">
                                     <table class="table table-striped table-bordered table-hover taller-kardex">
                                         <thead>
@@ -466,14 +489,14 @@
                                             @if ($tallerPractico->tallerKardex->respuestaTallerKardexUsuarioAutenticado() === null)
                                                 @for ($i = 0; $i < 2; $i++)
                                                     <tr>
-                                                        <td class="text-center vcenter td-dia numero" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">0</td>
+                                                        <td class="text-center vcenter td-dia numero " contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">0</td>
                                                         <td class="text-center vcenter td-mes numero" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">0</td>
                                                         <td class="text-center vcenter td-ano numero" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">0</td>
                                                         <td class="text-center vcenter td-detalle" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar."></td>
-                                                        <td class="text-center vcenter td-valor-unitario" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">$ 0</td>
-                                                        <td class="text-center vcenter td-entradas-cantidad numero" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">0</td>
+                                                        <td class="text-center vcenter td-valor-unitario actualizar-entradas-y-salidas" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">$ 0</td>
+                                                        <td class="text-center vcenter td-entradas-cantidad numero actualizar-entradas-y-salidas" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">0</td>
                                                         <td class="text-center vcenter td-entradas-valor">$ 0</td>
-                                                        <td class="text-center vcenter td-salidas-cantidad numero" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">0</td>
+                                                        <td class="text-center vcenter td-salidas-cantidad numero actualizar-entradas-y-salidas" contenteditable="true" data-toggle="tooltip" title="Presione clic para editar.">0</td>
                                                         <td class="text-center vcenter td-salidas-valor">$ 0</td>
                                                         <td class="text-center vcenter td-saldo-cantidad numero">0</td>
                                                         <td class="text-center vcenter td-saldo-valor">$ 0</td>
@@ -1238,7 +1261,213 @@
                 tabla.find('tbody').append(primerFilaClonada);
                 darFomatoACamposTallerKardex();
             });
+            $('.tab-content:visible').find(".taller-kardex").on('blur', '.actualizar-entradas-y-salidas', function(event) {
+                var elemento = this;
+                actualizarEntradasSalidasSaldoYPromedio(elemento)
+                    .then(function () {
+                        return darFomatoACamposTallerKardex();
+                    });
+            });
+            $('.tab-content:visible').find(".solucionar-taller-kardex").click(function(event) {
+                event.preventDefault();
+                var botonPulsado = $(this);
+                var tabPanelActivo = botonPulsado.parents('div.tab-pane');
+                var articulo = tabPanelActivo.find('input[name=articulo_taller_kardex]').val();
+                var direccion = tabPanelActivo.find('input[name=direccion_taller_kardex]').val();
+                var proveedores = tabPanelActivo.find('input[name=proveedores_taller_kardex]').val();
+                if(articulo == undefined || articulo == ""){
+                    swal(
+                        '¡Cuidado!',
+                        'El campo artículo es requerido.',
+                        'warning'
+                    );
+                    return;
+                }else if (direccion == undefined || direccion == "") {
+                    swal(
+                        '¡Cuidado!',
+                        'El campo dirección es requerido.',
+                        'warning'
+                    );
+                    return;
+                }else if (proveedores == undefined || proveedores == "") {
+                    swal(
+                        '¡Cuidado!',
+                        'El campo proveedores es requerido.',
+                        'warning'
+                    );
+                    return;
+                }else {
+                    var tabla = tabPanelActivo.find('table.taller-kardex');
+                    var textoOriginal = botonPulsado.text();
+                    botonPulsado.attr('disabled', true).text('ENVIANDO DATOS...');
+                    botonPulsado.parents("div").find(".adicionar-fila-kardex").attr('disabled', true);
+                    var ruta = botonPulsado.data("ruta");
+                    var inputFileImage = tabPanelActivo.find('input[type=file]');
+                    var archivo = inputFileImage[0].files[0];
+                    var filas = [];
+                    tabla.find('tbody > tr').each(function(index, el) {
+                        var fila = new Object();
+                        var tr = $(this);
+                        var dia = parseInt(numeral(tr.find('.td-dia').text()).format('0'));
+                        var mes = parseInt(numeral(tr.find('.td-mes').text()).format('0'));
+                        var ano = parseInt(numeral(tr.find('.td-ano').text()).format('0'));
+                        var detalle = tr.find('.td-detalle').text();
+                        var valorUnitario = parseInt(numeral(tr.find('.td-valor-unitario').text()).format('0'));
+                        var entradasCantidad = parseInt(numeral(tr.find('.td-entradas-cantidad').text()).format('0'));
+                        var entradasValor = parseInt(numeral(tr.find('.td-entradas-valor').text()).format('0'));
+                        var salidasCantidad = parseInt(numeral(tr.find('.td-salidas-cantidad').text()).format('0'));
+                        var salidasValor = parseInt(numeral(tr.find('.td-salidas-valor').text()).format('0'));
+                        var saldoCantidad = parseInt(numeral(tr.find('.td-saldo-cantidad').text()).format('0'));
+                        var saldoValor = parseInt(numeral(tr.find('.td-saldo-valor').text()).format('0'));
+                        var promedio = parseInt(numeral(tr.find('.td-promedio').text()).format('0'));
+                        if((detalle == undefined || detalle == "")){
+                            if (tabla.find('tbody > tr').length > 1) {
+                                tr.remove();
+                            }
+                            return;
+                        }else{
+                            var fila = {
+                                "dia" : dia,
+                                "mes" : mes,
+                                "ano" : ano,
+                                "detalle" : detalle,
+                                "valorUnitario" : valorUnitario,
+                                "entradasCantidad" : entradasCantidad,
+                                "entradasValor" : entradasValor,
+                                "salidasCantidad" : salidasCantidad,
+                                "salidasValor" : salidasValor,
+                                "saldoCantidad" : saldoCantidad,
+                                "saldoValor" : saldoValor,
+                                "promedio" : promedio
+                            };
+                            filas.push(fila);
+                        }
+                    });
+                    var data = new FormData();
+                    data.append('articulo',articulo);
+                    data.append('direccion',direccion);
+                    data.append('proveedores',proveedores);
+                    data.append('archivo_taller_kardex',archivo);
+                    data.append('filas',JSON.stringify(filas));
+                    var xhr =
+                        $.ajax({
+                            url: ruta,
+                            type: 'POST',
+                            headers: {'X-CSRF-TOKEN': '{{ csrf_token() }}' },
+                            dataType: 'JSON',
+                            //necesario para subir archivos via ajax
+                            cache: false,
+                            contentType: false,
+                            processData: false,
+                            data: data,
+                            beforeSend: function () {
+                            },
+                            success: function(data) {
+                                console.log(data);
+                                if(data.state == "error"){
+                                    swal({
+                                        title: '¡Error!',
+                                        text: data.message,
+                                        type: 'error'
+                                    });
+                                }else if(data.state == "success"){
+                                    swal({
+                                        title: '¡Éxito!',
+                                        text: data.message,
+                                        type: 'success'
+                                    });
+                                }
+                                if(data.archivo != null){
+                                    tabPanelActivo.find('.alert-link').attr('href', data.archivo.rear_rutaarchivo).text(data.archivo.rear_nombre);
+                                }
+                            }
+                        })
+                        .done(function() {
+                            console.log("success");
+                        })
+                        .fail(function() {
+                            console.log("error");
+                        })
+                        .always(function() {
+                            console.log("complete");
+                            botonPulsado.attr('disabled', false).text(textoOriginal);
+                            botonPulsado.parents("div").find(".adicionar-fila-kardex").attr('disabled', false);;
+                        });
+                }
+            });
+            function actualizarEntradasSalidasSaldoYPromedio(elemento) {
+                var promise = new Promise(function (resolve, reject) {
+                    var filaActual       = $(elemento).parents('tr');
+                    var tabla            = filaActual.parents('table.taller-kardex');
+                    var indiceFila       = filaActual.index();
+                    if (indiceFila > 0 && $(elemento).hasClass('td-valor-unitario') && filaActual.children('.td-salidas-cantidad').text() != '0') {
+                        swal(
+                            '¡Cuidado!',
+                            'Está modificando el valor unitario de una salida, cuando este debe tomar el valor del promedio de la fila anterior.<br><br>Para poder modificar este valor, debe cambiar la cantidad en salidas a 0.',
+                            'warning'
+                        );
+                        var filaAnterior = tabla.find('tbody > tr').eq(indiceFila - 1);
+                        var promedioAnterior = filaAnterior.children('.td-promedio').text();
+                        filaActual.children('.td-valor-unitario').text(promedioAnterior);
+                        resolve(elemento);
+                        return;
+                    }
+                    if (indiceFila > 0 && $(elemento).hasClass('td-entradas-cantidad') && filaActual.children('.td-salidas-cantidad').text() != '0') {
+                        swal(
+                            '¡Cuidado!',
+                            'Está modificando la cantidad de entradas en una fila que tiene salidas, no pueden existir entradas y salidas en la misma fila.<br><br>Para poder modificar este valor, debe cambiar la cantidad en salidas a 0, ó bien, adicione una nueva fila para su entrada.',
+                            'warning'
+                        );
+                        filaActual.children('.td-entradas-cantidad').text('0');
+                        resolve(elemento);
+                        return;
+                    }
+                    if ($(elemento).hasClass('td-salidas-cantidad') && filaActual.children('.td-entradas-cantidad').text() != '0') {
+                        swal(
+                            '¡Cuidado!',
+                            'Está modificando la cantidad de salidas en una fila que tiene entradas, no pueden existir entradas y salidas en la misma fila.<br><br>Para poder modificar este valor, debe cambiar la cantidad en entradas a 0, ó bien, adicione una nueva fila para su salida.',
+                            'warning'
+                        );
+                        filaActual.children('.td-salidas-cantidad').text('0');
+                        resolve(elemento);
+                        return;
+                    }
+                    if(indiceFila > 0 && $(elemento).hasClass('td-salidas-cantidad') && $(elemento).text() != '0'){
+                        var filaAnterior = tabla.find('tbody > tr').eq(indiceFila - 1);
+                        var promedioAnterior = filaAnterior.children('.td-promedio').text();
+                        console.log(filaAnterior.children('.td-promedio'));
+                        console.log(promedioAnterior);
+                        filaActual.children('.td-valor-unitario').text(promedioAnterior);
+                    }
+                    var valorUnitario    = parseInt(numeral(filaActual.children('.td-valor-unitario').text()).format('0'));
+                    var entradasCantidad = parseInt(numeral(filaActual.children('.td-entradas-cantidad').text()).format('0'));
+                    var salidasCantidad  = parseInt(numeral(filaActual.children('.td-salidas-cantidad').text()).format('0'));
 
+                    var entradasValor    = valorUnitario * entradasCantidad;
+                    var salidasValor     = valorUnitario * salidasCantidad;
+                    filaActual.children('.td-entradas-valor').text(entradasValor);
+                    filaActual.children('.td-salidas-valor').text(salidasValor);
+                    var saldoCantidad, saldoValor;
+                    if(indiceFila == 0){
+                        filaActual.children('.td-saldo-cantidad').text(entradasCantidad);
+                        filaActual.children('.td-saldo-valor').text(entradasValor);
+                        saldoCantidad = filaActual.children('.td-saldo-cantidad').text();
+                        saldoValor    = filaActual.children('.td-saldo-valor').text();
+                    }else{
+                        var filaAnterior = tabla.find('tbody > tr').eq(indiceFila - 1);
+                        var saldoCantidadFilaAnterior = parseInt(numeral(filaAnterior.children('.td-saldo-cantidad').text()).format('0'));
+                        var saldoValorFilaAnterior    = parseInt(numeral(filaAnterior.children('.td-saldo-valor').text()).format('0'));
+                        var saldoCantidad             = saldoCantidadFilaAnterior + entradasCantidad - salidasCantidad;
+                        var saldoValor                = saldoValorFilaAnterior + entradasValor - salidasValor;
+                    }
+                    var promedio = saldoValor / saldoCantidad;
+                    filaActual.children('.td-saldo-cantidad').text(saldoCantidad);
+                    filaActual.children('.td-saldo-valor').text(saldoValor);
+                    filaActual.children('.td-promedio').text(numeral(promedio).format('$0,0'));
+                    resolve(elemento);
+                });
+                return promise;
+            }
             function darFomatoACamposTallerKardex() {
                 $(".taller-kardex > tbody > tr > td").each(function(index, el) {
                     if ($(el).hasClass('td-detalle') || $(el).hasClass('td-opcion')) {
@@ -1251,7 +1480,6 @@
                     }
                 });
             }
-
             $('body').tooltip({
                 'selector': '[data-toggle="tooltip"]',
                 'container' : 'body'
