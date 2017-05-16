cbf9899fa75370fb195dfaba882a2a197b63cabe
Andrés David Montoya Aguirre <admont28@gmail.com>
2017-05-16 10:54:35 -0500

    Organización de rutas para la autenticación, registro, y recuperación de contraseña.

--- routes/web.php
+++ routes/web.php
@@ -263,38 +263,21 @@ Route::group(['prefix' => 'profesor', 'middleware' => ['auth','profesor']], func
 
 });
 
-
-// Authentication Routes...
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-// Registration Routes...
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@register');
-
-// Password Reset Routes...
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
-Route::get('/home', 'HomeController@index');
-
+Auth::routes();
 
 // ruta para asignar el valor del Lenguaje
 Route::group(['middleware' => ['web']], function () {
 
-//Route::get('/lang/{lang}', function () {
-//    return view('front.index');
-//});
+    //Route::get('/lang/{lang}', function () {
+    //    return view('front.index');
+    //});
 
-Route::get('lang/{lang}', function ($lang) {
-    session(['lang' => $lang]);
-    return \Redirect::back();
-})->where([
-    'lang' => 'en|es'
-]);
+    Route::get('lang/{lang}', function ($lang) {
+        session(['lang' => $lang]);
+        return \Redirect::back();
+    })->where([
+        'lang' => 'en|es'
+    ]);
 
 });
 ?>
